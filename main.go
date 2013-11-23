d5cfedebf8243748386c4a75942f37c813e30403
Juuso Haavisto <juuso@mail.com>
2013-11-23 15:19:13 +0200

    404 handler + go fmt

--- main.go
+++ main.go
@@ -6,19 +6,19 @@ import (
 )
 
 func middleware(w http.ResponseWriter, r *http.Request) (newW http.ResponseWriter, newR *http.Request) {
-  // use this if your site doesnt need anything else than HTTP GET
-  if r.Method != "GET" {
-    http.Redirect(w, r, "https://twitter.com/YOUR_TWITTER_HANDLER", 302)
-    return
-  }
-  //w.Header().Set("X-Hacker", "Hi.")
-  return
+	// use this if your site doesnt need anything else than HTTP GET
+	if r.Method != "GET" {
+		http.Redirect(w, r, "https://twitter.com/YOUR_TWITTER_HANDLER", 302)
+		return
+	}
+	//w.Header().Set("X-Hacker", "Hi.")
+	return
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
-  r.HandleFunc("/p/{page}", pageHandler)
+	r.HandleFunc("/p/{page}", pageHandler)
 	r.PathPrefix("/static/").Handler(http.FileServer(http.Dir(".")))
 	http.ListenAndServe(":8080", r)
 }
